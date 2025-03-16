34f345f6406a41ab65722a00a0c019db7dbfc8e0
EitanBellaiche <166850453+EitanBellaiche@users.noreply.github.com>
2025-03-17 00:57:04 +0200

    Update AdminPage.jsx (change server calls)

--- client/src/AdminPage.jsx
+++ client/src/AdminPage.jsx
@@ -29,7 +29,10 @@ Chart.register(...registerables);
 
 
 
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_URL || "http://localhost:5000"
+    : process.env.REACT_APP_API_PRODUCTION_URL || "https://spms-project.onrender.com";
 
   useEffect(() => {
     if (activeSection === "users") {
